51cddc9d99c9717d45166217e1c11931b7d23463
Palakorn Radarit <paradari@z2t6c1.42bangkok.com>
2024-03-04 16:05:27 +0700

    created

---
+++ push_swap.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paradari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 15:17:55 by paradari          #+#    #+#             */
+/*   Updated: 2024/03/04 15:18:13 by paradari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	nb;
+	int	neg;
+
+	i = 0;
+	nb = 0;
+	neg = 1;
+	while (str[i] == '\n' || str[i] == '\t' || str[i] == '\r' || str[i] == '\f'
+		|| str[i] == '\v' || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		nb = (nb * 10) + (str[i++] - '0');
+	return (nb * neg);
+}
+
+t_intlst	*atointlst(t_intlst list, char *argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		
+	}
+}
+
+void	push_swap(int argc, char *argv)
+{
+	t_intlst	list;
+
+	list = atointlst(list, argv);
+}
---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paradari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 15:32:55 by paradari          #+#    #+#             */
+/*   Updated: 2024/03/04 15:32:58 by paradari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <unistd.h>
+
+typedef struct int_list
+{
+	int				num;
+	struct int_list	*next;
+}					t_intlst;
+
+#endif
